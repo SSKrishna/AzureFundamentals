5bda6875224e6916f819ffe5a8dc04526d7609d5
Siddhartha Gunturu <gunturu.siddhartha@gmail.com>
2024-01-01 21:03:30 -0500

    updated EF core version to make other packages support
    so far we have created couple of Azure Function where the first one will write the sales request to Queue with HTTPTrigger and the other one will catch the request in Queue using QueueTrigger

---
+++ AzureFunction/Data/AzureTangyDbContext.cs
@@ -0,0 +1,24 @@
+﻿using AzureFunction.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AzureFunction.Data
+{
+    public class AzureTangyDbContext: DbContext
+    {
+        public AzureTangyDbContext(DbContextOptions<AzureTangyDbContext> options): base(options)
+        {
+
+        }
+
+        public DbSet<SalesRequest> SalesRequests { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<SalesRequest>(
+                entity => { entity.HasKey(c => c.Id);
+                });
+        }
+    }
+}
---
+++ AzureFunction/Startup.cs
@@ -0,0 +1,28 @@
+﻿using AzureFunction;
+using AzureFunction.Data;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+[assembly: WebJobsStartup(typeof(Startup))]
+namespace AzureFunction
+{
+    public class Startup : IWebJobsStartup
+    {
+        public void Configure(IWebJobsBuilder builder)
+        {
+            string connectionString = Environment.GetEnvironmentVariable("AzureSqlDatabase");
+
+            builder.Services.AddDbContext<AzureTangyDbContext>(
+             options =>
+             {
+                 options.UseSqlServer(connectionString);
+             });
+
+            builder.Services.BuildServiceProvider();
+        }
+    }
+}
+
